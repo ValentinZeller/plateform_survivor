8f14eb24a1c6d644442079c9aabc60f0aacb02cc
Valentin <valentin.zeller@celsius-online.com>
2023-04-24 15:23:07 +0200

    Pause menu

--- Assets/Scripts/Player/PlayerHUD.cs
+++ Assets/Scripts/Player/PlayerHUD.cs
@@ -6,6 +6,7 @@ namespace PlateformSurvivor.Player
 {
     public class PlayerHUD : MonoBehaviour
     {
+        [SerializeField] private GameObject pause;
         [SerializeField] private GameObject setting;
         [SerializeField] private TextMeshProUGUI playerHP;
         [SerializeField] private TextMeshProUGUI playerCoin;
@@ -39,17 +40,13 @@ namespace PlateformSurvivor.Player
             text = minute + " : " + second;
             return text;
         }
-        public void OpenSetting()
+
+        public void Pause(InputAction.CallbackContext ctx)
         {
-            Time.timeScale = !setting.activeSelf ? 0f : 1f;
-            setting.SetActive(!setting.activeSelf);
-        }
-        public void OpenSetting(InputAction.CallbackContext ctx)
-        {
-            if (ctx.performed)
+            if (ctx.performed && stat.GetHealth() > 0 && !setting.activeSelf)
             {
-                Time.timeScale = !setting.activeSelf ? 0f : 1f;
-                setting.SetActive(!setting.activeSelf);
+                Time.timeScale = !pause.activeSelf ? 0f : 1f;
+                pause.SetActive(!pause.activeSelf);
             }
         }
     }
